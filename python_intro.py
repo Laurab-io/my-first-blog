edf1427b6751596792c6af7796906504fdda4b44
laurab-io <io.laurab@gmail.com>
2016-10-22 13:11:04 -0600

    added url path

---
+++ python_intro.py
@@ -0,0 +1,54 @@
+# def hi(name):
+#     print('Hi ' + name + '!')
+#
+#
+# girls = ["Betsy", "Shelly", "Ola", "You"]
+#
+# for name in girls:
+#     hi (name)
+#     print('Next girl')
+
+
+# for i in range (1,6):
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def hi():
+#    print ('Hi there!')
+#    print ('Hows it hanging')
+
+#hi()
+#
+# def hi(name):
+#     if name == "Ola":
+#         print('Hi Ola')
+#     elif name == "Sunny":
+#         print('Hi Sunny')
+#     else:
+#         print('Hi whomever!')
+#
+# hi("Sunny")
+
+
+# def hi(name):
+#     print("Hi " + name + "!")
+#
+# hi("Sheila")
